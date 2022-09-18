469e567164e7f8f3917c40fd9559f4a1489b6040
Maria Gallego <mariagestionredes@gmail.com>
2022-09-18 20:21:07 +0200

    create home and login routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React, { useState, createContext, useContext } from "react";
 import { app } from "./firebase/index";
 import "./index.css";
 import Header from "./components/Header";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+
+export const AppContext = createContext(null);
 
 function App() {
+  const [route, setRoute] = useState("home");
   return (
-    <>
+    <AppContext.Provider value={{ route, setRoute }}>
       <Header />
       <main className="p-6">
-      Bienvenida a FireShopping!
+        {route === "home" && <Home />}
+        {route === "login" && <Login />}
       </main>
-    </>
+    </AppContext.Provider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import { ImFire } from 'react-icons/im';
+import React, { useContext } from "react";
+import { ImFire } from "react-icons/im";
+import { AppContext } from "../App";
 
 const Header = () => {
+  const { route, setRoute } = useContext(AppContext);
   return (
     <header className="h-20  w-full bg-gray-100 shadow-lg flex items-center justify-between px-8">
-    <div className="flex items-center gap-2">
-    <ImFire className="text-2xl text-pink-500" />
-    <p className="text-xl font-semibold text-pink-500">FireShopping</p>
-    </div>
-    <button className="bg-sky-600 text-white py-1 px-3 rounded-full hover:bg-sky-800 transition">Login</button>
+      <div className="flex items-center gap-2 cursor-pointer" onClick={() => setRoute('home')}>
+        <ImFire className="text-2xl text-pink-500" />
+        <p className="text-xl font-semibold text-pink-500 hover:text-pink-700">FireShopping</p>
+      </div>
+      <button 
+      className="bg-sky-600 text-white py-1 px-3 rounded-full hover:bg-sky-800 transition"
+      onClick={() => setRoute('login')}>
+        Login
+      </button>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
